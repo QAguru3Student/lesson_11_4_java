65ae4d20588281d644101bb977e542cc0427f7e8
Babushkin Anton <napishi_antonu@mail.ru>
2022-03-30 17:20:31 +0300

    added examples

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
                 x,
                 y,
                 z;
+        long l = 30_000_000_000L;
         byte d = 127,
                 e = 21,
                 f = 1;
@@ -23,15 +24,16 @@ public class FirstClass {
         System.out.println("z"+ "=" + z);
         x = a + b;
         System.out.println("x" + "=" + x );
-        System.out.println(e + d);
         System.out.println("e-- =" + (e--));
         System.out.println("--e =" + (--e));
         System.out.println("b == d =" + (b == d));
         System.out.println("c != e =" + (c != e));
-        System.out.println("c - g = " + (c - g));
         System.out.println("h && i && j = " + (h && i && j));
         System.out.println("i|| j&& k = " + (i || j && k) );
         System.out.println("!(j&& k) || h = " + (!(j && k) || k));
+        System.out.println("byte >max = " + (e + d));
+        System.out.println("byte <min = " + (e - 300));
+        System.out.println("int + float = " + (c - g)); // int + float
+        System.out.println("int + long = " + (b + l));
     }
-
 }
